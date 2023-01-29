18ce6d8b44c449eb34f0674c688b2c4d431bedae
NegusNati <negusnati@gmail.com>
2023-01-29 11:33:18 -0800

    local storage configured

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 //Init weather object
 
-const weather = new Weather("London");
 const ui = new UI();
+const localStore = new LocalStorage();
+
+//get stored location data
+
+const currentLoc = localStore.getLocationData();
+
+
+const weather = new Weather(currentLoc.city);
+
 
 //change location event
+
+
 
 document.getElementById('w-change-btn').addEventListener('click', (e)=>{
 
     const city = document.getElementById('city').value;
 
+    //change location
     weather.changeLocation(city);
 
+    //set new location to LS
+    localStore.setLocationData(city);
     
 
     //call get weather for the new data
---
+++ localStorage.js
@@ -0,0 +1,19 @@
+class LocalStorage {
+  constructor() {
+    this.city;
+    this.defaultCity = "Addis Ababa";
+  }
+
+  getLocationData() {
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem("city");
+    }
+
+    return {city: this.city};
+  }
+  setLocationData(city) {
+    localStorage.setItem("city", city);
+  }
+}
