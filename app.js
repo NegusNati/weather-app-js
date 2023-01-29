607ce007aeec41e4145e8c4cb49dd113e0871602
NegusNati <negusnati@gmail.com>
2023-01-29 11:16:05 -0800

    query a new city

--- app.js
+++ app.js
@@ -3,6 +3,26 @@
 const weather = new Weather("London");
 const ui = new UI();
 
+//change location event
+
+document.getElementById('w-change-btn').addEventListener('click', (e)=>{
+
+    const city = document.getElementById('city').value;
+
+    weather.changeLocation(city);
+
+    
+
+    //call get weather for the new data
+    getWeather();
+
+    //colse modal using jQuery
+    // $('#locModal').modal().hide();
+    // $('#locModal').modal('hide');
+    $('#locModal').modal('toggle');
+
+});
+
 //Get weather when DOM load
 
 document.addEventListener("DOMContentLoaded", getWeather());
